d4bcf8e14e2dc5782323e6c5581314966c8b22f1
tuncaulubilge <tunca.ulubilge@gmail.com>
2016-06-19 20:32:29 +0300

    Configs

--- app/config/config.js
+++ app/config/config.js
@@ -3,7 +3,7 @@
 var links = require('docker-links').parseLinks(process.env)
 
 links.postgres = links.postgres || {hostname: 'localhost', port: 5432}
-links.redis = links.redis || {hostname: 'localhost', port: 5432}
+links.redis = links.redis || {hostname: 'localhost', port: 6379}
 links.queue = links.queue || {hostname: 'localhost', port: 11300}
 
 module.exports = {
